2bbb431956fc993d29965eecd4ffa4f13584ffea
Guvanch99 <awediyewguwanc@gmail.com>
2022-03-14 22:02:07 +0300

    add disabled

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,11 +1,21 @@
 import S from "./Navigation.module.css";
 import {TripleDotsIcon} from "../icons/TripleDotsIcon";
-import {useState} from "react";
+import {SyntheticEvent, useState} from "react";
 import {NavLink} from "react-router-dom";
+import {useTodos} from "../../hook/useTodos";
 
 const Navigation=()=> {
   const [isActive, setActive]=useState<boolean>(false)
+  const {filteredTodos}=useTodos()
+  const isCompleted=filteredTodos("COMPLETED").length
+  const isRemoved=filteredTodos('REMOVED').length
 
+  const handleNavigate=(disabled:boolean)=>{
+   return (event:SyntheticEvent)=>{
+     !disabled&&event.preventDefault()
+     setActive(false)
+   }
+  }
 return(
   <div className={S.container}>
   <div className={isActive?S.iconWrapperActive:S.iconWrapper}>
@@ -13,9 +23,24 @@ return(
     {
       isActive&&(
         <div className={S.statusContainer}>
-          <NavLink to='/completed' className={S.text}>Completed</NavLink>
-          <NavLink to='/' className={S.text}>In Progress</NavLink>
-          <NavLink to='/removed' className={S.text}>Removed</NavLink>
+          <NavLink
+            onClick={handleNavigate(!!isCompleted)}
+            to='/completed'
+            className={!!isCompleted ? S.text : S.disabled}>
+            Completed
+          </NavLink>
+          <NavLink
+            to='/'
+            onClick={()=>setActive(false)}
+            className={S.text}>
+            In Progress
+          </NavLink>
+          <NavLink
+            onClick={handleNavigate(!!isRemoved)}
+            to='/removed'
+            className={!!isRemoved ? S.text : S.disabled}>
+            Removed
+          </NavLink>
         </div>
       )
     }
